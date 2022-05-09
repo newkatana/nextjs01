3600a79ecc7c10f8b1814b0d4ee843900ea209c4
newkatana <kikyukatana@gmail.com>
2022-05-09 17:26:04 +0700

    as

--- pages/attractions.tsx
+++ pages/attractions.tsx
@@ -1,3 +1,5 @@
+let baseUrl = process.env.Vercel_URL
+
 import Link from 'next/link'
 import { InferGetServerSidePropsType } from 'next'
 
@@ -13,7 +15,7 @@ type Data = {
  }
 
 export const getServerSideProps = async () => {
-  const res = await fetch('http://localhost:3000/api/attractions')
+  const res = await fetch(baseUrl +'/api/attractions')
   const data: Data[] = await res.json()
 
   return {
