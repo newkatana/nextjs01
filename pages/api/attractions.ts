d084f775400213ace3e4c9e4db39c76cc452229d
newkatana <56946347+newkatana@users.noreply.github.com>
2022-05-10 00:00:23 +0700

    Update attractions.ts

--- pages/api/attractions.ts
+++ pages/api/attractions.ts
@@ -5,10 +5,10 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
+  host: '203.157.229.35',
+  user: 'root',
+  password: 'adminhdc',
+  database: 'nextjs_fullstack',
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export
