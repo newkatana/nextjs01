782767203abff908826a31712f493920600912db
newkatana <56946347+newkatana@users.noreply.github.com>
2022-05-10 00:40:45 +0700

    Update attractions.ts

--- pages/api/attractions.ts
+++ pages/api/attractions.ts
@@ -5,10 +5,10 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: `process.env.DB_HOST`,
-  user: `process.env.DB_USER`,
-  password: `process.env.DB_PASS`,
-  database: `process.env.DB_DATABASE`
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export
